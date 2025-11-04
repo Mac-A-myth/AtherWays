195d02ce2f840f845520cf31820df8a959173a74
Mac-A-myth <amithmangathaya@gmail.com>
2025-11-04 15:01:22 +0530

    Bean Commit

--- src/com/ust/frs/bean/ReservationBean.java
+++ src/com/ust/frs/bean/ReservationBean.java
@@ -1,5 +1,83 @@
 package com.ust.frs.bean;
 
 public class ReservationBean {
+	private String reservationID,userID,scheduleID,reservationType,bookingDate,journeyDate;
+	private int noOfSeats,bookingStatus;
+	private double totalFare;
+	public ReservationBean(String reservationID, String userID, String scheduleID, String reservationType,
+			String bookingDate, String journeyDate, int noOfSeats, int bookingStatus, double totalFare) {
+		super();
+		this.reservationID = reservationID;
+		this.userID = userID;
+		this.scheduleID = scheduleID;
+		this.reservationType = reservationType;
+		this.bookingDate = bookingDate;
+		this.journeyDate = journeyDate;
+		this.noOfSeats = noOfSeats;
+		this.bookingStatus = bookingStatus;
+		this.totalFare = totalFare;
+	}
+	@Override
+	public String toString() {
+		return "ReservationBean [reservationID=" + reservationID + ", userID=" + userID + ", scheduleID=" + scheduleID
+				+ ", reservationType=" + reservationType + ", bookingDate=" + bookingDate + ", journeyDate="
+				+ journeyDate + ", noOfSeats=" + noOfSeats + ", bookingStatus=" + bookingStatus + ", totalFare="
+				+ totalFare + "]";
+	}
+	public String getReservationID() {
+		return reservationID;
+	}
+	public void setReservationID(String reservationID) {
+		this.reservationID = reservationID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getScheduleID() {
+		return scheduleID;
+	}
+	public void setScheduleID(String scheduleID) {
+		this.scheduleID = scheduleID;
+	}
+	public String getReservationType() {
+		return reservationType;
+	}
+	public void setReservationType(String reservationType) {
+		this.reservationType = reservationType;
+	}
+	public String getBookingDate() {
+		return bookingDate;
+	}
+	public void setBookingDate(String bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+	public String getJourneyDate() {
+		return journeyDate;
+	}
+	public void setJourneyDate(String journeyDate) {
+		this.journeyDate = journeyDate;
+	}
+	public int getNoOfSeats() {
+		return noOfSeats;
+	}
+	public void setNoOfSeats(int noOfSeats) {
+		this.noOfSeats = noOfSeats;
+	}
+	public int getBookingStatus() {
+		return bookingStatus;
+	}
+	public void setBookingStatus(int bookingStatus) {
+		this.bookingStatus = bookingStatus;
+	}
+	public double getTotalFare() {
+		return totalFare;
+	}
+	public void setTotalFare(double totalFare) {
+		this.totalFare = totalFare;
+	}
+	
 
 }
--- src/com/ust/frs/bean/ScheduleBean.java
+++ src/com/ust/frs/bean/ScheduleBean.java
@@ -1,5 +1,59 @@
 package com.ust.frs.bean;
 
 public class ScheduleBean {
-
+   private  String scheduleID,flightID,routeID,availableDays,departureTime;
+   private int travelDuration;
+   public ScheduleBean(String scheduleID, String flightID, String routeID, String availableDays, String departureTime,
+		int travelDuration) {
+	super();
+	this.scheduleID = scheduleID;
+	this.flightID = flightID;
+	this.routeID = routeID;
+	this.availableDays = availableDays;
+	this.departureTime = departureTime;
+	this.travelDuration = travelDuration;
+   }
+   @Override
+   public String toString() {
+	return "ScheduleBean [scheduleID=" + scheduleID + ", flightID=" + flightID + ", routeID=" + routeID
+			+ ", availableDays=" + availableDays + ", departureTime=" + departureTime + ", travelDuration="
+			+ travelDuration + "]";
+   }
+   public String getScheduleID() {
+	return scheduleID;
+   }
+   public void setScheduleID(String scheduleID) {
+	this.scheduleID = scheduleID;
+   }
+   public String getFlightID() {
+	return flightID;
+   }
+   public void setFlightID(String flightID) {
+	this.flightID = flightID;
+   }
+   public String getRouteID() {
+	return routeID;
+   }
+   public void setRouteID(String routeID) {
+	this.routeID = routeID;
+   }
+   public String getAvailableDays() {
+	return availableDays;
+   }
+   public void setAvailableDays(String availableDays) {
+	this.availableDays = availableDays;
+   }
+   public String getDepartureTime() {
+	return departureTime;
+   }
+   public void setDepartureTime(String departureTime) {
+	this.departureTime = departureTime;
+   }
+   public int getTravelDuration() {
+	return travelDuration;
+   }
+   public void setTravelDuration(int travelDuration) {
+	this.travelDuration = travelDuration;
+   }
+   
 }
